602e81b0dd41dd65b380eb0ced408810bc0e310d
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2021-07-24 12:55:25 -0500

    suma de elmeentos

---
+++ JavaBasicModule/src/clase1/Numeros.java
@@ -0,0 +1,18 @@
+package clase1;
+
+public class Numeros {
+
+	public static void main(String[] args) {
+		//int a = 4;
+		//int b = 5;
+		//int c = a + b;
+		int a = 4, b = 5, c = a + b; // Declaracion e inicializacion en una misma linea
+		
+		//System.out.println("Valor de a = " + a);
+		//System.out.println("Valor de b = " + b);
+		//System.out.println("Valor de c = " + c);
+		
+		// Utilizamos caracter de escape \n que hace un salto de linea
+		System.out.println("Valor de a = " + a + "\nValor de b = " + b + "\nValor de c = " + c);
+	}
+}
