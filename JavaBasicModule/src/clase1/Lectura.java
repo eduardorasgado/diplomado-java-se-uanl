f442c216ac0753269d912663f688b77838a612f3
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2021-07-24 13:41:33 -0500

    leer de consola y operaciones matematicas

---
+++ JavaBasicModule/src/clase1/Lectura.java
@@ -0,0 +1,18 @@
+package clase1;
+
+import java.util.Scanner;
+
+public class Lectura {
+	public static void main(String[] args) {
+		String nombre, apellido;
+		Scanner lector = new Scanner(System.in); // Instanciando un objeto
+		// Pedimos valores
+		System.out.println("Ingrese su nombre: ");
+		nombre = lector.nextLine();
+		System.out.println("Ingrese su apellido");
+		apellido = lector.nextLine();
+		// Mensaje de bienvenida
+		System.out.println("Bienvenido a Java, " + nombre + " " + apellido + "!");
+		lector.close();
+	}
+}
