2be34e081c61d79d93b100ed2636787230d1c750
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2021-07-31 11:01:43 -0500

    se crean getters y setters

--- JavaBasicModule/src/clase2/Empleado.java
+++ JavaBasicModule/src/clase2/Empleado.java
@@ -1,21 +1,52 @@
 package clase2;
 
 public class Empleado {
-	// variables de instancia o de clase
-	private String nombre, puesto;
-	private int numEmpleado;
-	private double salario;
-	
-	public Empleado() {
-		nombre = puesto = "";
-		numEmpleado = 0;
-		salario = 0.0;
-	}
-	
-	public Empleado(String n, String p, int num, double s) {
-		nombre = n;
-		puesto = p;
-		numEmpleado = num;
-		salario = s;
-	}
+  // variables de instancia o de clase
+  private String nombre, puesto;
+  private int numEmpleado;
+  private double salario;
+
+  public Empleado() {
+    nombre = puesto = "";
+    numEmpleado = 0;
+    salario = 0.0;
+  }
+
+  public Empleado(String nombre, String puesto, int numEmpleado, double salario) {
+    this.nombre = nombre;
+    this.puesto = puesto;
+    this.numEmpleado = numEmpleado;
+    this.salario = salario;
+  }
+
+  public Empleado(String nombre) {
+    this.nombre = nombre;
+    puesto = "Nuevo Empleado";
+    numEmpleado = 0;
+    salario = 0.0;
+  }
+
+  // GETTERS
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public String getPuesto() {
+    return puesto;
+  }
+
+  public int getNumEmpleado() {
+    return numEmpleado;
+  }
+
+  // SETTERS
+
+  public void setPuesto(String puesto) {
+    this.puesto = puesto;
+  }
+
+  public void setSalario(double salario) {
+    this.salario = salario;
+  }
 }
