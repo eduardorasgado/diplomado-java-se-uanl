d5efe22476e79cd80ab48c45e2ac2d3858c20c6f
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2021-08-10 18:36:43 -0500

    proyecto integrador terminado

--- JavaBasicModule/src/CalculadoraMatricial/CalculadoraMatricial.java
+++ JavaBasicModule/src/CalculadoraMatricial/CalculadoraMatricial.java
@@ -1,7 +1,10 @@
 /** */
 package CalculadoraMatricial;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
+
+import static java.util.Objects.nonNull;
 
 /**
  * Clase para realizar operaciones básicas de algebra lineal
@@ -10,6 +13,7 @@ import java.util.Scanner;
  */
 public class CalculadoraMatricial {
   private MatrixHandler matrix1, matrix2;
+  private int[][] resultMatrix;
   private Scanner scanner;
   private int operation;
   private boolean calculatorState;
@@ -19,13 +23,15 @@ public class CalculadoraMatricial {
     calculatorState = true;
   }
 
-  public void printOperationMenu() {
+  /** Imprime las operaciones que se tienen disponibles en la calculadora matricial */
+  private void printOperationMenu() {
     System.out.println("Las operaciones disponibles son las siguientes");
     System.out.println("1. Suma");
     System.out.println("2. Resta");
     System.out.println("3. Multiplicación");
   }
 
+  /** Menu inicial y bucle donde el usuario ingresa la operacion a realizar */
   public void runMain() {
     System.out.println("---Calculadora de matrices---");
     while (calculatorState) {
@@ -43,6 +49,10 @@ public class CalculadoraMatricial {
     operation = scanner.nextInt();
   }
 
+  /**
+   * Logica principal para pedir los datos de matrices, realizar las operaciones e imprimirlas en
+   * pantalla, luego de que ya se tenga la operacion a realizar
+   */
   private void runOperationContext() {
     String opBaseMessage = "Ha elegido la operación: ";
     boolean validOperation = false;
@@ -55,35 +65,71 @@ public class CalculadoraMatricial {
       System.out.println();
       switch (operation) {
         case 1:
-          System.out.println(opBaseMessage.concat("SUMA"));
-          System.out.println("Datos de Matriz 1: ");
-          matrix1.getMatrixData(ColumnRowPattern.EQUAL);
-          System.out.println("Datos de Matriz 2: ");
-          matrix2.getMatrixData(ColumnRowPattern.EQUAL);
-          matrix1.add(matrix2);
+          getMatrices(opBaseMessage.concat("SUMA"));
+          resultMatrix = matrix1.add(matrix2);
+          if (nonNull(resultMatrix)) showResults();
+
           validOperation = true;
           break;
         case 2:
-          System.out.println(opBaseMessage.concat("RESTA"));
-          System.out.println("Datos de Matriz 1: ");
-          matrix1.getMatrixData(ColumnRowPattern.EQUAL);
-          System.out.println("Datos de Matriz 2: ");
-          matrix2.getMatrixData(ColumnRowPattern.EQUAL);
-          matrix1.substract(matrix2);
+          getMatrices(opBaseMessage.concat("RESTA"));
+          resultMatrix = matrix1.substract(matrix2);
+          if (nonNull(resultMatrix)) showResults();
           validOperation = true;
           break;
         case 3:
-          System.out.println(opBaseMessage.concat("MULTIPLICACIÓN"));
-          System.out.println("Datos de Matriz 1: ");
-          matrix1.getMatrixData(ColumnRowPattern.DIFFERENT);
-          System.out.println("Datos de Matriz 2: ");
-          matrix2.getMatrixData(ColumnRowPattern.DIFFERENT);
-          matrix1.product(matrix2);
+          getMatrices(opBaseMessage.concat("MULTIPLICACIÓN"));
+          resultMatrix = matrix1.product(matrix2);
+          if (nonNull(resultMatrix)) showResults();
           validOperation = true;
           break;
         default:
           System.out.println("Parece que no existe esta operación, intente de nuevo.");
       }
     }
+  }
+
+  /**
+   * Pide al usuario los datos de la matriz 1 y 2
+   *
+   * @param header texto para imprimir
+   */
+  private void getMatrices(String header) {
+    System.out.println(header);
+    try {
+      System.out.println("Datos de Matriz 1: ");
+      matrix1.getMatrixData();
+      System.out.println("Datos de Matriz 2: ");
+      matrix2.getMatrixData();
+    } catch (Exception e) {
+      System.out.println(
+          "Error al insertar datos de matriz. "
+              + "Vuelva a iniciar la calculadora de nuevo para poder continuar.");
+    }
+  }
+
+  /**
+   * Muestra la matriz que se le pasa por parametro, formateandola con llaves
+   *
+   * @param matrix matriz a mostrar
+   */
+  private void showMatrix(int[][] matrix) {
+    for (int i = 0; i < matrix.length; i++) {
+      System.out.print("[ ");
+      for (int j = 0; j < matrix[0].length; j++) {
+        System.out.print(matrix[i][j] + " ");
+      }
+      System.out.println("]");
+    }
+  }
+
+  /** Muestra en texto la matriz 1 y 2 y expone el resultado */
+  private void showResults() {
+    System.out.println("Matriz 1: ");
+    showMatrix(matrix1.getMatrix());
+    System.out.println("Matriz 2: ");
+    showMatrix(matrix2.getMatrix());
+    System.out.println("La matriz resultado es: ");
+    showMatrix(resultMatrix);
   }
 }
--- JavaBasicModule/src/CalculadoraMatricial/MatrixHandler.java
+++ JavaBasicModule/src/CalculadoraMatricial/MatrixHandler.java
@@ -1,5 +1,6 @@
 package CalculadoraMatricial;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -12,6 +13,10 @@ public class MatrixHandler {
   private int columns;
   private int rows;
   private Scanner scanner;
+  private final String SUCCESS_OPERATION_MSG = "Operación realizada con éxito";
+  private final String FAILED_EQUAL_OPERATION =
+      "Las filas y columnas de las matrices son diferentes entre matrices. "
+          + "No es posible de realiza la operación";
 
   public MatrixHandler() {}
 
@@ -33,34 +38,148 @@ public class MatrixHandler {
     matrix = new int[columns][rows];
   }
 
-  public void getMatrixData(ColumnRowPattern columnRowPatter) {
-    if (columnRowPatter.equals(ColumnRowPattern.EQUAL)) {
-      System.out.println("Insertar número de columnas/filas: ");
-      columns = rows = scanner.nextInt();
-    } else if (columnRowPatter.equals(ColumnRowPattern.DIFFERENT)) {
-      System.out.println("Insertar número de filas:");
-      rows = scanner.nextInt();
-      System.out.println("Insertar número de columnas: ");
-      columns = scanner.nextInt();
-    } else {
+  /**
+   * Solicita y captura los datos de la matriz del objeto que invoca el metodo
+   *
+   * @throws InputMismatchException Puede darse un error de escaneo, ingresar letra en vez de numero
+   */
+  public void getMatrixData() throws InputMismatchException {
 
+    System.out.println("Insertar número de filas:");
+    rows = scanner.nextInt();
+    System.out.println("Insertar número de columnas: ");
+    columns = scanner.nextInt();
+
+    matrix = new int[rows][columns];
+
+    for (int i = 0; i < matrix.length; i++) {
+      //
+      System.out.println("---- Insertar elementos de fila " + (i + 1) + ": ");
+      for (int j = 0; j < matrix[0].length; j++) {
+        System.out.println("---- > columna " + (j + 1) + ": ");
+        matrix[i][j] = scanner.nextInt();
+      }
     }
-    // TODO: Realizar la operacion en otro metodo y llamarlo
-    System.out.println("Insertar elementos de fila #: ");
+    System.out.println();
   }
 
-  public void add(MatrixHandler otherMatrix) {
-    // TODO: Desarrollar metodo
-    System.out.println("Operación realizada con éxito");
+  /**
+   * Realiza la operacion de suma entre una matriz A del objeto actual y una matriz B
+   *
+   * @param otherMatrix matriz B
+   * @return matriz resultante o nulo
+   */
+  public int[][] add(MatrixHandler otherMatrix) {
+    if (validateOperation(ColumnRowPattern.EQUAL, otherMatrix)) {
+      int[][] resultMatrix = new int[otherMatrix.getRows()][otherMatrix.getColumns()];
+      for (int i = 0; i < matrix.length; i++) {
+        for (int j = 0; j < matrix[0].length; j++) {
+          resultMatrix[i][j] = matrix[i][j] + otherMatrix.getMatrix()[i][j];
+        }
+      }
+      System.out.println(SUCCESS_OPERATION_MSG);
+      return resultMatrix;
+    } else {
+      System.out.println(FAILED_EQUAL_OPERATION);
+      return null;
+    }
   }
 
-  public void substract(MatrixHandler otherMatrix) {
-    // TODO: Desarrollar metodo
-    System.out.println("Operación realizada con éxito");
+  /**
+   * Realiza la operacion de resta entre la matriz A del objeto actual y una matriz B
+   *
+   * @param otherMatrix matriz B
+   * @return matriz resultado o nulo
+   */
+  public int[][] substract(MatrixHandler otherMatrix) {
+    if (validateOperation(ColumnRowPattern.EQUAL, otherMatrix)) {
+      int[][] resultMatrix = new int[otherMatrix.getRows()][otherMatrix.getColumns()];
+      for (int i = 0; i < matrix.length; i++) {
+        for (int j = 0; j < matrix[0].length; j++) {
+          resultMatrix[i][j] = matrix[i][j] - otherMatrix.getMatrix()[i][j];
+        }
+      }
+      System.out.println(SUCCESS_OPERATION_MSG);
+      return resultMatrix;
+    } else {
+      System.out.println(FAILED_EQUAL_OPERATION);
+      return null;
+    }
   }
 
-  public void product(MatrixHandler otherMatrix) {
-    // TODO: Desarrollar metodo
-    System.out.println("Operación realizada con éxito");
+  /**
+   * realiza la operacion de multiplicacion entre la matriz del objeto actual A y una matriz B.
+   *
+   * @param otherMatrix matriz B
+   * @return matriz resultado o nulo
+   */
+  public int[][] product(MatrixHandler otherMatrix) {
+    if (validateOperation(ColumnRowPattern.DIFFERENT, otherMatrix)) {
+      int[][] resultMatrix = new int[rows][otherMatrix.getColumns()];
+
+      for (int i = 0; i < resultMatrix.length; i++) {
+        for (int j = 0; j < resultMatrix[0].length; j++) {
+          for (int k = 0; k < columns; k++) {
+            resultMatrix[i][j] += matrix[i][k] * otherMatrix.getMatrix()[k][j];
+          }
+        }
+      }
+
+      System.out.println(SUCCESS_OPERATION_MSG);
+      return resultMatrix;
+    } else {
+      System.out.println("No es posible obtener el producto entre estas matrices");
+      return null;
+    }
+  }
+
+  /**
+   * Valida que dado un tipo de patron de columnas y filas entre dos matrices, estas puedan realizar
+   * la operacion necesaria. Para suma y resta es necesario que las matrices tengan mismo numero de
+   * columnas y filas y para producto de matrices es necesario que el numero de columnas de la
+   * matriz A(1) sea igual al numero de filas de la matriz B(2)
+   *
+   * @param pattern define como se validaran las matrices 1 y 2
+   * @param otherMatrixHandler Matriz B
+   * @return
+   */
+  private boolean validateOperation(ColumnRowPattern pattern, MatrixHandler otherMatrixHandler) {
+    if (pattern.equals(ColumnRowPattern.EQUAL)) {
+      return rows == otherMatrixHandler.getRows() && columns == otherMatrixHandler.getColumns();
+
+    } else if (pattern.equals(ColumnRowPattern.DIFFERENT)) {
+      return columns == otherMatrixHandler.getRows();
+    }
+    return false;
+  }
+
+  /** @return the columns */
+  protected int getColumns() {
+    return columns;
+  }
+
+  /** @param columns the columns to set */
+  protected void setColumns(int columns) {
+    this.columns = columns;
+  }
+
+  /** @return the rows */
+  protected int getRows() {
+    return rows;
+  }
+
+  /** @param rows the rows to set */
+  protected void setRows(int rows) {
+    this.rows = rows;
+  }
+
+  /** @return the matrix */
+  protected int[][] getMatrix() {
+    return matrix;
+  }
+
+  /** @param matrix the matrix to set */
+  protected void setMatrix(int[][] matrix) {
+    this.matrix = matrix;
   }
 }
